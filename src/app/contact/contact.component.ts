a3f1689b048b6183b87d68e5f9e852363efeaeda
Lenwr <saidabou.lateam@gmail.com>
2023-07-27 18:42:25 +0200

    cont

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  email = new FormControl('', [Validators.required, Validators.email]);
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Vous devez entrer une valeur';
+    }
+
+    return this.email.hasError('email') ? 'Non valide' : '';
+  }
+
 }
+
+
